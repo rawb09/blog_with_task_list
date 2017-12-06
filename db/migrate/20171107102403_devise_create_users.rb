d2290484f109d01b77b4ecf6d509287c6889a40e
Dream Corps <dreamcorps@Spare-MacBook-Air-4.local>
2017-12-05 17:20:30 -0800

    Updated Database

--- db/migrate/20171107102403_devise_create_users.rb
+++ db/migrate/20171107102403_devise_create_users.rb
@@ -20,10 +20,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string   :last_sign_in_ip
 
       # Confirmable
-      t.string :confirmed_at
+      t.string  :confirmed_at
       t.string   :confirmation_token
       add_column :users, :confirmed_at, :datetime
-      # t.datetime :confirmed_at
+      t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
--- db/migrate/20171110003355_add_user_id_to_todos.rb
+++ db/migrate/20171110003355_add_user_id_to_todos.rb
@@ -1,5 +1,5 @@
 class AddUserIdToTodos < ActiveRecord::Migration[5.1]
   def change
-    add_column :todos, :user_id, :integer
+    add_column :todos, :user_id, :integer, :confirmed_at
   end
 end
